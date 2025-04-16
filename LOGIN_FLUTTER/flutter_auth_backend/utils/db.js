9084ca64ab12a2f011dc3928850f7ffd917a691f
ashish6298 <ashishgoswami6298@gmail.com>
2025-04-16 12:00:49 +0530

    `Update MongoDB connection URL in db.js`

--- LOGIN_FLUTTER/flutter_auth_backend/utils/db.js
+++ LOGIN_FLUTTER/flutter_auth_backend/utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://ashish:goswami@cluster0.trds1g8.mongodb.net/carelink', {
+    await mongoose.connect('mongodb link', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
